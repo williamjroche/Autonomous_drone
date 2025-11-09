121d8f034e0adbbcba3c1524a51d2bcc8c26e0b4
William Roche <74993912+williamjroche@users.noreply.github.com>
2025-11-08 19:18:04 -0500

    Add files via upload
    added a line of code for clarity of first time use

--- flight_controller/main.py
+++ flight_controller/main.py
@@ -1,9 +1,12 @@
 import esc_control as esc
 import time
 
-#initialization and arming the escs/motor
+#initialization and arming the ESCs/motor
 esc.esc_init()
+#esc.esc_calibrate() #for first time use, ESCs need to be calibrated --> uncomment this line of code-->take off propellors-->run file-->delete this line of code
 esc.esc_arm()
+
+
 
 #test motor
 esc.esc_speed(1500000, 1)
